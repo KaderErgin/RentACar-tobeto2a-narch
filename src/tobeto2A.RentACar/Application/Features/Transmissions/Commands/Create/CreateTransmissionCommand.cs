994bbe2ff703b7b10259088fa61cbf94c66ede03
kader <kader.ergin.37@gmail.com>
2024-03-02 17:22:16 +0300

    Brand-Fuel-Transmission-App-WebApi-Added

---
+++ src/tobeto2A.RentACar/Application/Features/Transmissions/Commands/Create/CreateTransmissionCommand.cs
@@ -0,0 +1,46 @@
+﻿using Application.Features.Brands.Rules;
+using Application.Features.Transmissions.Rules;
+using Application.Services.Repositories;
+using AutoMapper;
+using Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Transmissions.Commands.Create;
+public class CreateTransmissionCommand : IRequest<CreatedTransmissionResponse>
+{
+    public string Name { get; set; }
+
+
+
+    // Inner class
+    public class CreateTransmissionCommandHandler : IRequestHandler<CreateTransmissionCommand, CreatedTransmissionResponse>
+    {
+        private readonly ITransmissionRepository _transmissionRepository;
+        private readonly IMapper _mapper;
+        private readonly TransmissionBusinessRules _transmissionBusinessRules;
+
+        public CreateTransmissionCommandHandler(ITransmissionRepository transmissionRepository, IMapper mapper, TransmissionBusinessRules transmissionBusinessRules)
+        {
+            _transmissionRepository = transmissionRepository;
+            _mapper = mapper;
+            _transmissionBusinessRules = transmissionBusinessRules;
+        }
+
+        public async Task<CreatedTransmissionResponse> Handle(CreateTransmissionCommand request, CancellationToken cancellationToken)
+        {
+            await _transmissionBusinessRules.CarShouldNotExistsWithSameName(request.Name);
+            Transmission transmission = _mapper.Map<Transmission>(request);
+
+            Transmission addedTransmission = await _transmissionRepository.AddAsync(transmission);
+
+            CreatedTransmissionResponse createdTransmissionResponse = _mapper.Map<CreatedTransmissionResponse>(addedTransmission);
+
+            return createdTransmissionResponse;
+        }
+    }
+}
---
+++ src/tobeto2A.RentACar/Application/Features/Transmissions/Rules/TransmissionBusinessRules.cs
@@ -0,0 +1,29 @@
+﻿using Application.Services.Repositories;
+using Domain.Entities;
+using NArchitecture.Core.Application.Rules;
+using NArchitecture.Core.CrossCuttingConcerns.Exception.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Transmissions.Rules;
+public class TransmissionBusinessRules : BaseBusinessRules
+{
+    private readonly ITransmissionRepository _transmissionRepository;
+
+    public TransmissionBusinessRules(ITransmissionRepository transmissionRepository)
+    {
+        _transmissionRepository = transmissionRepository;
+    }
+
+
+    public async Task CarShouldNotExistsWithSameName(string name)
+    {
+        Transmission? transmissionWithSameName = await _transmissionRepository.GetAsync(b => b.Name == name);
+
+        if (transmissionWithSameName is not null)
+            throw new BusinessException("Aynı isme sahip bir vites zaten mevcut.");
+    }
+}
